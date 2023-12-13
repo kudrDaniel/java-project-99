b71f8d522443451c47fcae11ecda95a670958ab8
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-12-13 13:03:37 +0300

    Update application.yml, Dockerfile. Add profiles: application-development.yml, application-production.yml. Add model User

---
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,43 @@
+package hexlet.code.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    @CreatedDate
+    private LocalDate createdAt;
+
+    @LastModifiedDate
+    private LocalDate updatedAt;
+}
