48b8a43fface0778df4bafd8b18419c77cd12a7c
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-12-22 17:11:31 +0300

    Remove unused imports

--- src/main/java/hexlet/code/dto/TaskCreateDTO.java
+++ src/main/java/hexlet/code/dto/TaskCreateDTO.java
@@ -2,7 +2,6 @@ package hexlet.code.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
