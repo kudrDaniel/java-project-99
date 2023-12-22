8b9c36c0a81cad6fd6b5204bbd0a521f375e1564
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-12-22 15:14:35 +0300

    Update RsaKeyProperties

--- src/main/java/hexlet/code/component/RsaKeyProperties.java
+++ src/main/java/hexlet/code/component/RsaKeyProperties.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.interfaces.RSAPrivateKey;
@@ -14,6 +15,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
+import java.util.Optional;
 
 @Getter
 @Component
@@ -27,6 +29,9 @@ public class RsaKeyProperties {
             @Value(value = "${rsa.public-key}") String publicKey,
             @Value(value = "${rsa.private-key}") String privateKey
     ) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        publicKey = tryGetKeyFromFile(publicKey).orElse(publicKey);
+        privateKey = tryGetKeyFromFile(privateKey).orElse(privateKey);
+
         var keyFactory = KeyFactory.getInstance("RSA");
 
         var publicKeyEncoded = Base64.getDecoder().decode(publicKey);
@@ -37,5 +42,19 @@ public class RsaKeyProperties {
 
         rsaPublicKey = (RSAPublicKey) keyFactory.generatePublic(publicKeySpec);
         rsaPrivateKey = (RSAPrivateKey) keyFactory.generatePrivate(privateKeySpec);
+    }
+
+    private static Optional<String> tryGetKeyFromFile(String path) {
+        path = path.replace("classpath:", "");
+        try (var is = RsaKeyProperties.class.getClassLoader().getResourceAsStream(path)) {
+            var content = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+            content = content.replace("-----BEGIN PRIVATE KEY-----", "");
+            content = content.replace("-----BEGIN PUBLIC KEY-----", "");
+            content = content.replace("-----END PRIVATE KEY-----", "");
+            content = content.replace("-----END PUBLIC KEY-----", "");
+            return Optional.of(content.replace("\n", ""));
+        } catch (Exception ex) {
+            return Optional.empty();
+        }
     }
 }
