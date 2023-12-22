7d7ddc45e98ef6b6f0eb40f9a0ad67d5ab29af1b
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-12-22 13:27:03 +0300

    Update Application name

--- src/main/java/hexlet/code/app/AppApplication.java
+++ src/main/java/hexlet/code/app/AppApplication.java
@@ -8,9 +8,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
-public class Application {
+public class AppApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AppApplication.class, args);
     }
 
     @Bean
