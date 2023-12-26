0359265dda19ebb25815852e93f11c26a4f3feb2
kudrDaniel <kudr.daniel.vlad.ch@gmail.com>
2023-12-26 17:09:50 +0300

    Test fix github actions reading secrets and vars

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class AppApplication {
     public static void main(String[] args) {
+        System.out.println(System.getenv("RSA_PUBLIC"));
         SpringApplication.run(AppApplication.class, args);
     }
 
